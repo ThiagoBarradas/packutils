0cf747e6c3c93e4c698941787298eb5bc83f0faa
Leonardo Jorge <leogojorge@outlook.com>
2019-08-15 18:09:03 -0300

    :sparkles: Add custom mongo enum serializer

---
+++ PackUtils/MongoSerializer/EnumAsDescriptionSerializer.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization;
+using System;
+
+namespace PackUtils.MongoSerializer
+{
+    public class EnumAsDescriptionSerializer<TEnum> : IBsonSerializer
+    {
+        public Type ValueType => typeof(TEnum);
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        {
+            var valueEnum = (Enum)Enum.Parse(typeof(TEnum), value.ToString());
+            context.Writer.WriteString(EnumUtility.GetDescriptionFromEnum(valueEnum));
+        }
+
+        object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var value = context.Reader.ReadString();
+            return EnumUtility.GetEnumFromDescription<TEnum>(value);
+        }
+
+    }
