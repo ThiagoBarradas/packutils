35d972bde40848b15f2863afb679910ffccf4c6e
Thiago Barradas <th.barradas@gmail.com>
2019-11-05 17:12:58 -0300

    :art: implements assembly utility with generic params

---
+++ PackUtils/AssemblyUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace PackUtils
+{
+    public static class AssemblyUtility
+    {
+        private static void InvokeMethodWithGeneric(this object instance, string method, Type type, object[] parameters = null)
+        {
+            MethodInfo baseMethod = instance.GetType().GetMethod(method);
+            MethodInfo methodWithGeneric = baseMethod.MakeGenericMethod(type);
+            methodWithGeneric.Invoke(instance, parameters);
+        }
+
+        private static object CreateInstanceWithGeneric(Type baseType, Type genericType, object[] parameters = null)
+        {
+            Type[] typeArgs = { genericType };
+            var makeme = baseType.MakeGenericType(typeArgs);
+
+            return Activator.CreateInstance(makeme, parameters);
+        }
+    }
+}
