738e8930e6ec443df337a7eb436e8d49f3f13dd0
Thiago Barradas <th.barradas@gmail.com>
2019-11-12 16:41:57 -0300

    :art: add invoke generic with return

--- PackUtils/AssemblyUtility.cs
+++ PackUtils/AssemblyUtility.cs
@@ -12,6 +12,13 @@ namespace PackUtils
             methodWithGeneric.Invoke(instance, parameters);
         }
 
+        public static object InvokeMethodWithGenericAndReturn(this object instance, string method, Type type, object[] parameters = null)
+        {
+            MethodInfo baseMethod = instance.GetType().GetMethod(method);
+            MethodInfo methodWithGeneric = baseMethod.MakeGenericMethod(type);
+            return methodWithGeneric.Invoke(instance, parameters);
+        }
+
         public static object CreateInstanceWithGeneric(Type baseType, Type genericType, object[] parameters = null)
         {
             Type[] typeArgs = { genericType };
