24fcceca54fe78273ccbcc70ed13b41503ade160
Thiago Barradas <th.barradas@gmail.com>
2020-01-24 11:11:08 -0300

    :arrow_up: update all dependencies

--- PackUtils/SwaggerUtility.cs
+++ PackUtils/SwaggerUtility.cs
@@ -1,29 +1,30 @@
-﻿using Swashbuckle.AspNetCore.SwaggerGen;
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Linq;
 
 namespace PackUtils
 {
     public class SnakeEnumSchemaFilter : Swashbuckle.AspNetCore.SwaggerGen.ISchemaFilter
     {
-        public void Apply(Swashbuckle.AspNetCore.Swagger.Schema model, SchemaFilterContext context)
-        {   
-            model.Enum = model.Enum?.Select(p => p.ToString().ToSnakeCase()).ToArray();
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            schema.Enum = schema.Enum.Select(r => OpenApiAnyFactory.CreateFor(schema, r.ToString().ToSnakeCase())).ToArray();
         }
     }
 
     public class CamelEnumSchemaFilter : Swashbuckle.AspNetCore.SwaggerGen.ISchemaFilter
     {
-        public void Apply(Swashbuckle.AspNetCore.Swagger.Schema model, SchemaFilterContext context)
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
         {
-            model.Enum = model.Enum?.Select(p => p.ToString().ToCamelCase()).ToArray();
+            schema.Enum = schema.Enum.Select(r => OpenApiAnyFactory.CreateFor(schema, r.ToString().ToCamelCase())).ToArray();
         }
     }
 
     public class LowerEnumSchemaFilter : Swashbuckle.AspNetCore.SwaggerGen.ISchemaFilter
     {
-        public void Apply(Swashbuckle.AspNetCore.Swagger.Schema model, SchemaFilterContext context)
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
         {
-            model.Enum = model.Enum?.Select(p => p.ToString().ToLowerCase()).ToArray();
+            schema.Enum = schema.Enum.Select(r => OpenApiAnyFactory.CreateFor(schema, r.ToString().ToLowerCase())).ToArray();
         }
     }
 }
